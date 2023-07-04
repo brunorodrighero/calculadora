e2cc8c43acc5f27e6c252b3daad0e3f8584c9235
Bruno Luiz Silva Rodighiero <66458327+brunorodrighero@users.noreply.github.com>
2023-07-04 17:13:25 -0400

    Fixed and Update main.js - All the problems with the logic were solved.

--- main.js
+++ main.js
@@ -1,90 +1,166 @@
-let resultado = document.querySelector(".resultado");
+let resultado = document.querySelector('.resultado');
 let operadorSelecionado;
-let valor;
-let valorFinal;
+let valor1 = '';
+let valor2 = '';
 
-document.getElementById("ac").addEventListener("click", botaoAcClicado);
-document.getElementById("positivo-negativo").addEventListener("click", inverterSinalNumero);
-document.getElementById("porcentagem").addEventListener("click", converterEmPorcentagem);
-document.getElementById("virgula").addEventListener("click", botaoNumeroClicado);
-document.getElementById("zero").addEventListener("click", botaoNumeroClicado);
-document.getElementById("um").addEventListener("click", botaoNumeroClicado);
-document.getElementById("dois").addEventListener("click", botaoNumeroClicado);
-document.getElementById("tres").addEventListener("click", botaoNumeroClicado);
-document.getElementById("quatro").addEventListener("click", botaoNumeroClicado);
-document.getElementById("cinco").addEventListener("click", botaoNumeroClicado);
-document.getElementById("seis").addEventListener("click", botaoNumeroClicado);
-document.getElementById("sete").addEventListener("click", botaoNumeroClicado);
-document.getElementById("oito").addEventListener("click", botaoNumeroClicado);
-document.getElementById("nove").addEventListener("click", botaoNumeroClicado);
-document.getElementById("igual").addEventListener("click", realizarOperacaoIgual);
-document.getElementById("divisao").addEventListener("click", botaoOperacaoClicado);
-document.getElementById("multiplicacao").addEventListener("click", botaoOperacaoClicado);
-document.getElementById("subtracao").addEventListener("click", botaoOperacaoClicado);
-document.getElementById("adicao").addEventListener("click", botaoOperacaoClicado);
+document.getElementById('ac').addEventListener('click', botaoAcClicado);
+document
+  .getElementById('positivo-negativo')
+  .addEventListener('click', inverterSinalNumero);
+document
+  .getElementById('porcentagem')
+  .addEventListener('click', converterEmPorcentagem);
+document
+  .getElementById('virgula')
+  .addEventListener('click', botaoVirgulaClicado);
 
-function botaoAcClicado(){
-  resultado.textContent = "0";
+document.getElementById('zero').addEventListener('click', botaoNumeroClicado);
+document.getElementById('um').addEventListener('click', botaoNumeroClicado);
+document.getElementById('dois').addEventListener('click', botaoNumeroClicado);
+document.getElementById('tres').addEventListener('click', botaoNumeroClicado);
+document.getElementById('quatro').addEventListener('click', botaoNumeroClicado);
+document.getElementById('cinco').addEventListener('click', botaoNumeroClicado);
+document.getElementById('seis').addEventListener('click', botaoNumeroClicado);
+document.getElementById('sete').addEventListener('click', botaoNumeroClicado);
+document.getElementById('oito').addEventListener('click', botaoNumeroClicado);
+document.getElementById('nove').addEventListener('click', botaoNumeroClicado);
+
+document
+  .getElementById('igual')
+  .addEventListener('click', realizarOperacaoIgual);
+document
+  .getElementById('divisao')
+  .addEventListener('click', botaoOperacaoClicado);
+document
+  .getElementById('multiplicacao')
+  .addEventListener('click', botaoOperacaoClicado);
+document
+  .getElementById('subtracao')
+  .addEventListener('click', botaoOperacaoClicado);
+document
+  .getElementById('adicao')
+  .addEventListener('click', botaoOperacaoClicado);
+
+function botaoAcClicado() {
+  resultado.textContent = '0';
   operadorSelecionado = undefined;
-  valor = 0;
-  valorFinal = undefined;
+  valor1 = '';
+  valor2 = '';
 }
 
 function botaoNumeroClicado() {
-  if (this.textContent == "," && resultado.textContent.includes(",")) return;
-  if (resultado.textContent == "0" || operadorSelecionado) {
-    resultado.textContent = this.textContent;
-    operadorSelecionado = false;
+  if (resultado.textContent.length >= 9 && this.textContent !== '.') return;
+  if (resultado.textContent === '0' || (operadorSelecionado && valor2 === '')) {
+    updateDisplay(this.textContent);
+    if (operadorSelecionado) {
+      valor2 = this.textContent;
+    } else {
+      valor1 = this.textContent;
+    }
   } else {
+    updateDisplay(resultado.textContent + this.textContent);
+    if (operadorSelecionado) {
+      valor2 += this.textContent;
+    } else {
+      valor1 += this.textContent;
+    }
+  }
+}
+
+function botaoVirgulaClicado() {
+  if (!resultado.textContent.includes(',')) {
     resultado.textContent += this.textContent;
   }
 }
 
-function operacao(valor1, operador, valor2){
+function operacao(v1, operador, v2) {
   switch (operador) {
-    case "x":
-      return valor1 * valor2;
-    case "÷":
-      if (valor2 !== 0) {
-        return valor1 / valor2;
+    case 'x':
+      return v1 * v2;
+    case '÷':
+      if (v2 !== 0) {
+        return v1 / v2;
       } else {
         alert('Divisão por zero não é permitida');
-        return valor1;
+        return v1;
       }
-    case "+":
-      return valor1 + valor2;
-    case "-":
-      return valor1 - valor2;
+    case '+':
+      return v1 + v2;
+    case '-':
+      return v1 - v2;
     default:
-      return valor1;
+      return v1;
   }
 }
 
-function realizarOperacaoIgual() {  
-  if (operadorSelecionado) {
-    valorFinal = operacao(parseFloat(valor), operadorSelecionado, parseFloat(resultado.textContent.replace(",", ".")));
-    resultado.textContent = valorFinal.toString().replace(".", ",");
+function realizarOperacaoIgual() {
+  if (valor1 !== '' && operadorSelecionado !== undefined && valor2 !== '') {
+    valor1 = operacao(
+      parseFloat(valor1.replace(',', '.')),
+      operadorSelecionado,
+      parseFloat(valor2.replace(',', '.'))
+    )
+      .toString()
+      .replace('.', ',');
+    updateDisplay(valor1);
+    valor2 = '';
     operadorSelecionado = undefined;
   }
 }
 
-function botaoOperacaoClicado(){
-  if (operadorSelecionado) {
-    valorFinal = operacao(parseFloat(valor), operadorSelecionado, parseFloat(resultado.textContent.replace(",", ".")));
-    resultado.textContent = valorFinal.toString().replace(".", ",");
+function botaoOperacaoClicado() {
+  if (valor1 !== '' && valor2 !== '') {
+    valor1 = operacao(
+      parseFloat(valor1.replace(',', '.')),
+      operadorSelecionado,
+      parseFloat(valor2.replace(',', '.'))
+    )
+      .toString()
+      .replace('.', ',');
+    updateDisplay(valor1);
   }
-  valor = parseFloat(resultado.textContent.replace(",", "."));
-  operadorSelecionado = this.textContent;  
+  operadorSelecionado = this.textContent;
+  valor2 = '';
 }
 
-function inverterSinalNumero(){
-  if(resultado.textContent!="0"){
-    resultado.textContent = (parseFloat(resultado.textContent.replace(",", ".")) * -1).toString().replace(".", ",");
+function inverterSinalNumero() {
+  if (resultado.textContent !== '0') {
+    if (valor2 === '') {
+      valor1 = (parseFloat(valor1.replace(',', '.')) * -1)
+        .toString()
+        .replace('.', ',');
+      resultado.textContent = valor1;
+    } else {
+      valor2 = (parseFloat(valor2.replace(',', '.')) * -1)
+        .toString()
+        .replace('.', ',');
+      resultado.textContent = valor2;
+    }
   }
 }
 
-function converterEmPorcentagem(){
-  if(resultado.textContent!="0"){
-    resultado.textContent = (parseFloat(resultado.textContent.replace(",", ".")) / 100).toString().replace(".", ",");
+function converterEmPorcentagem() {
+  if (resultado.textContent != '0') {
+    resultado.textContent = (
+      parseFloat(resultado.textContent.replace(',', '.')) / 100
+    )
+      .toString()
+      .replace('.', ',');
+  }
+}
+
+function updateDisplay(text) {
+  if (text.includes(',')) {
+    let [whole, fractional] = text.split(',');
+    if (whole.length + fractional.length > 8) {
+      fractional = parseFloat('0.' + fractional)
+        .toFixed(8 - whole.length - 1)
+        .split('.')[1];
+      resultado.textContent = whole + ',' + fractional;
+    } else {
+      resultado.textContent = text;
+    }
+  } else {
+    resultado.textContent = text;
   }
 }
